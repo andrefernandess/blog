b24d1ea1c2f913cca0c74efa23d37d78ec1700ee
Andre Fernandes <andre.l.fernandess@hotmail.com>
2025-11-13 15:00:16 -0300

    Fixes and features to blog page

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,6 @@
 class BlogController < ApplicationController
+  layout "application_blank"
   def index
-    @posts = Post.where("publish_date <= ?", Time.zone.now).first
+    @posts = Post.where("publish_date <= ?", Time.zone.now).order("publish_date desc").first
   end
 end
